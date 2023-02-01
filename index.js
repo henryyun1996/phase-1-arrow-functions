363aadd16c70b14b811f4787a1cfa560180b8ea5
Henry <henrysungyun@gmail.com>
2023-01-31 16:34:51 -0800

    another one bites the dust

---
+++ index.js
@@ -0,0 +1,8 @@
+const divide = (parameter1, parameter2) => parameter1 / parameter2;
+divide(2000, 100);
+
+const square = x => x ** x;
+square(2);
+
+const add = (num1, num2) => num1 + num2;
+add(3, 4);
